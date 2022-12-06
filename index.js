2cc340837dd05d8b1ac560916cbe12dcee8cbed6
Tunisia Artope <artope@yahoo.com>
2022-12-06 01:01:38 +0000

    Added comments and Valid Parentheses solution

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
+//NYC Coders Workshop:
+
+//Creates a node structure with a value and next property
 class Node{
+  //construct the node object to assign key/value pairs
   constructor(value){
     this.value = value;
     this.next = null;
   }
 }
 
+//Creates Stack structure; Last In First Out (LIFO; the head is at the top)
 class Stack{
+  //construct the Stack with head and size
   constructor(){
     this.head = null;
     this.size = 0;
   }
 
+  //adds a value to top of Stack
+  //the method accepts a value parameter
   push(value){
-    //create a new node with value val
+    //create a new node
     const newHead = new Node(value);
-    //point the new node's next to head
+    //assign head to newHeads pointer
     newHead.next = this.head;
-    //update head to new node
+    //assign newHead to head
     this.head = newHead;
-    //increment size property
+    //increment size
     this.size++;
+    //return stack
     return this;
   }
-  
+
+  //remove and return value from top of Stack
+  //the method does not take parameters
   pop(){
     //edge case: if stack is empty, return null
     if(this.size === 0) return null;
@@ -32,26 +43,29 @@ class Stack{
     this.head = oldHead.next;
     //update oldHead pointer to null to disconnect from stack
     oldHead.next = null;
-    //decrement size property
+    //decrement size
     this.size--;
-    //return oldHead
+    //return oldHeads value
     return oldHead.value;
   }
-  
+
+  //returns value at the top of Stack
   peek(){
     //if stack empty, return null
     if(this.size === 0) return null;
-    //grab reference to head
-    // const grabbedHead = this.head;
-    //return head
+    //return heads value
     return this.head.value;
   }
-  
+
+  //checks if Stack is empty or not
   isEmpty(){
+    // returns Boolean if Stack size property is empty or not
     return this.size === 0
   }
-  
+
+  // returns the number of values in the Stack
   getSize(){
+    //return size property
     return this.size;
   }
 
@@ -80,22 +94,28 @@ stack.push(2);
 stack.push(3);
 stack.push(4);
 stack.push(5);
+// console.log(stack.pop());
 // console.log(stack.peek());
 // console.log(stack.isEmpty());
 // console.log(stack.getSize());
 
-
 // console.log(stack);
 // console.log(stack.printListArray()); 
+//_______________________________________________
 
+//Queue uses the same node class structure
+
+//Creates Queue structure; First In First Out (FIFO; the head is at the beginning of the queue, the tail is at the end of the queue)
 class Queue{
+  //construct the Queue with head, tail, and size
   constructor(){
     this.head = null;
     this.tail = null;
     this.size = 0;
   }
 
-  //adds to tail
+  //adds value to Queues tail
+  //the method accepts a value parameter
   enqueue(value){
     //create new node
     const newNode = new Node(value);
@@ -104,41 +124,54 @@ class Queue{
       this.head = newNode;
       this.tail = this.head;
     }
+    //assign new node to tails pointer
     this.tail.next = newNode;
+    //update tail to be the new node
     this.tail = newNode;
+    //increment the size
     this.size++;
+    //return Queue
     return this;
   }
-  
+
+  //removes and returns the value at the front of the Queue
   dequeue(){
     //return null if the queue is empty
     if(this.size === 0) return null;
-    //grab head reference
+    //grab reference to head
     const removedHead = this.head;
-    //update head reference to removedHead's next property
+    //update head to removedHead's next property
     this.head = removedHead.next;
     //update removedHead's next property to null to disconnect it
     removedHead.next = null;
-    //update tail reference if queue becomes empty
-    if(this.head === null){
-      this.tail = null;
-    }
     //decrement size
     this.size--;
-    //return removedHead value
+    //update head/tail reference if queue becomes empty
+    if(this.size === 0){
+      this.head = null;
+      this.tail = null;
+    }
+    //return removedHeads value
     return removedHead.value;
   }
-  
+
+  //returns the value at the beginning of the Queue
   peek(){
+    //edge case: if Queue is empty, return null
     if(this.size === 0) return null;
+    //return the heads value
     return this.head.value;
   }
-  
+
+  //checks if the Queue is empty or not
   isEmpty(){
+    // returns Boolean if Queue size property is empty or not
     return this.size === 0
   }
-  
+
+  //returns the number of values in the Queue
   getSize(){
+    //return size property
     return this.size;
   }
 
@@ -168,7 +201,79 @@ queue.enqueue(3);
 queue.enqueue(4);
 queue.enqueue(5);
 // console.log(queue.dequeue());
+// console.log(queue.peek());
 // console.log(queue.isEmpty());
-console.log(queue.getSize());
+// console.log(queue.getSize());
 
-console.log(queue.printListArray());
+// console.log(queue);
+// console.log(queue.printListArray());
+//_______________________________________________
+
+/*
+20. Valid Parentheses
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+1. Open brackets must be closed by the same type of brackets.
+2. Open brackets must be closed in the correct order.
+3. Every close bracket has a corresponding open bracket of the same type.
+
+Example 1:
+Input: s = "()"
+Output: true
+
+Example 2:
+Input: s = "()[]{}"
+Output: true
+
+Example 3:
+Input: s = "(]"
+Output: false  (doesn't meet requirement #1)
+
+Example 4:
+Input: s = "))"
+Output: false  (doesn't meet requirement #3)
+
+Example 5:
+Input: s = ")()"
+Output: false  (doesn't meet requirement #3)
+*/
+
+var isValid = function(s) {
+  //edge case: return false if odd number of characters
+  if(s.length % 2 !== 0) return false;
+  //create an empty stack; only open brackets go in the stack
+  const stack = [];
+  //create map to define matching pairs
+  const match = new Map([
+    ['(', ')'],
+    ['{', '}'],
+    ['[', ']']
+  ]);
+  //loop through s (for every 'bracket' of input 's')
+  for(const bracket of s){
+    //if open parentheses, push to stack (if match has the current element in a key, push to stack)
+    if(match.has(bracket)){
+      stack.push(bracket);
+    //else if it's a closed bracket, see if the pairs match )
+    }else{
+      //if stack not empty, compare with top (last) bracket
+      if(stack.length !== 0){
+        //grab top bracket in variable 
+        const lastOpen = stack.pop();
+        //if match keys' value (lastOpen's value) is not equal to current element, return false
+        if(match.get(lastOpen) !== bracket){
+          return false;
+        }
+      //otherwise if stack is empty, return false
+      }else{
+        return false;
+      }
+    }
+  }
+  //return true if stack is empty (stack must be empty at end of algo for parenthesis to be valid)
+  return stack.length === 0;
+};
+
+console.log(isValid(")()"));
